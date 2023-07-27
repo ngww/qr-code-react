06a16b0713c3f8b50cff220272aa94f3b68aa608
Sithembiso Ngwenya <ngww95@gmail.com>
2023-07-27 12:33:00 +0100

    QR Code rewrite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import qr from './images/image-qr-code.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(qr);
+
+const App = () => {
+    return (
+        <div className='body'>
+            <div className='main'>
+                <img src={qr} alt='QR Code' className='center' />
+                <h3>Improve your front-end skills by building projects</h3>
+                <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+
+            <div className='footer'>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/ngww">Thembi Ngwenya</a>.
+            </div>
+        </div>
+    );
 }
 
 export default App;
